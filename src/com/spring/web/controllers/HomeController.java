99f2f761f11183a326c6dc52e4beaab7c5cef0ce
Olga Kozlova <okozlova@kyriba.com>
2018-02-19 17:38:05 +0300

    122: Using Logging

--- src/com/spring/web/controllers/HomeController.java
+++ src/com/spring/web/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.spring.web.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	private static Logger logger = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showHome() {
+		logger.info("Show logging home...");
 		return "home";
 	}
 	
