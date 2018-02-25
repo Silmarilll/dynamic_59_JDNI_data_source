70420a5d4bdfed928d4efebd657df2aa3683fb9c
Olga Kozlova <olga.bsuir@gmail.com>
2018-02-25 14:17:06 +0300

    125+126: Coding the JUnit DAO Tests

---
+++ test/com/spring/web/test/tests/UserDaoTest.java
@@ -0,0 +1,53 @@
+package com.spring.web.test.tests;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.web.dao.User;
+import com.spring.web.dao.UsersDAO;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("dev")
+@ContextConfiguration(locations={
+		"classpath:com/spring/web/config/dao-context.xml",
+		"classpath:com/spring/web/config/security-context.xml",
+		"classpath:com/spring/web/test/config/datasource.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserDaoTest {
+	
+	@Autowired
+	private UsersDAO usersDao;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Before
+	public void init() {
+		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+		jdbc.execute("delete from users");
+		jdbc.execute("delete from authorities");		
+	}
+
+	
+	@Test
+	public void createUser() {
+		User user = new User("Name", "name@gmail.com", "1", true, "Offers");	
+		boolean result = usersDao.create(user);
+		List<User> users = usersDao.getAllUsers();
+		assertTrue("User creation should return true", result);
+		assertEquals(1, users.size());
+	}
+}
